1de3eba72af710ac43def382f8c92a59ad33b4de
Zhu Ming <mingzhu.z+github@gmail.com>
2019-10-28 14:20:31 +0800

    make a change

--- index.js
+++ index.js
@@ -63,5 +63,5 @@ app.get('/:shortUrl', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port ', app.get('port'));
 });
